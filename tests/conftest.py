ffe4ef96845e3467e0046057a518fb37d7bc290f
Patrick Deziel <deziel.patrick@gmail.com>
2023-06-09 18:51:47 -0500

    fix attempt 2

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,6 @@
+import pytest
+import asyncio
+
 ##########################################################################
 # Global pytest options
 ##########################################################################
@@ -7,3 +10,10 @@ def pytest_addoption(parser):
     parser.addoption(
         "--live", action="store_true", default=False, help="run live tests"
     )
+
+
+@pytest.fixture()
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
