389a81a9cd3f2b325d4a0c176f61d0b2dbb4e78e
Patrick Deziel <deziel.patrick@gmail.com>
2023-07-14 12:35:29 -0500

    version bump to v0.7.3

--- pyensign/version.py
+++ pyensign/version.py
@@ -5,9 +5,9 @@
 __version_info__ = {
     "major": 0,
     "minor": 7,
-    "micro": 2,
+    "micro": 3,
     "releaselevel": "beta",
-    "serial": 1,
+    "serial": 0,
 }
 
 ##########################################################################
