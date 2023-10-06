cabd119a27726516b83b28caaf0849797bcaefc1
Benjamin Bengfort <benjamin@bengfort.com>
2023-10-06 10:55:24 -0500

    enum docs and tests

--- pyensign/enum.py
+++ pyensign/enum.py
@@ -2,7 +2,31 @@ from enum import Enum
 from pyensign.api.v1beta1 import topic_pb2
 
 
-class TopicState(Enum):
+class PBEnum(Enum):
+    """
+    Base class for Python enumerations that map to protocol buffer enumeration
+    descriptors, providing methods for parsing and converting values.
+    """
+
+    @classmethod
+    def parse(cls, name):
+        """
+        Parse an protocol buffer enumeration value from a string (case and whitespace
+        insensitive parsing). Raises a KeyError if the string is not in the enumeration.
+        """
+        name = name.strip().upper()
+        return cls[name]
+
+    @classmethod
+    def convert(cls, pb_val, name):
+        """
+        Convert a protocol buffer descriptor or integer to the enumeration value.
+        """
+        pb_names = topic_pb2.DESCRIPTOR.enum_types_by_name[name].values_by_number
+        return cls[pb_names[pb_val].name]
+
+
+class TopicState(PBEnum):
     """
     TopicState enumerates the possible states that a topic might be in.
 
@@ -43,18 +67,47 @@ class TopicState(Enum):
     ALLOCATING = topic_pb2.TopicState.ALLOCATING
     REPAIRING = topic_pb2.TopicState.REPAIRING
 
-    @classmethod
-    def parse(cls, name):
-        name = name.strip().upper()
-        return cls[name]
+    __aliases__ = {}
 
     @classmethod
     def convert(cls, pb_val):
-        pb_names = topic_pb2.DESCRIPTOR.enum_types_by_name["TopicState"].names_by_value
-        return cls[pb_names[pb_val]]
+        return super(TopicState, cls).convert(pb_val, "TopicState")
 
 
-class DeduplicationStrategy(Enum):
+class DeduplicationStrategy(PBEnum):
+    """
+    DeduplicationStrategy enumerates the possible deduplication mechanisms for
+    determining if two events are duplicates of each other.
+
+    Strategies
+    ----------
+    UNKNOWN : 0
+        A topic should never have this policy, indicating something has gone wrong.
+
+    NONE : 1
+        No deduplication is performed, all events are stored when published.
+
+    STRICT : 2
+        Deduplication considers events with identical data, metadata, mimetype, and type
+        as being duplicates. All data is compared as is in a case-sensitive form.
+
+    DATAGRAM : 3
+        Deduplication considers events with identical data as being duplicates
+        regardless of the metadata, mimetype, or type.
+
+    KEY_GROUPED : 4
+        A deduplication policy that compares the data of two events so long as they have
+        the same values for specified keys (keys are required). E.g. an event is not a
+        duplicate if the metadata contains different "color" keys but identical data.
+
+    UNIQUE_KEY : 5
+        A deduplication policy that only compares the set of specified keys in the
+        metadata and does not consider data, mimetype, or type.
+
+    UNIQUE_FIELD : 6
+        A deduplication policy that only compares the set of specified fields in the
+        data. The data must be in a parseable representation by Ensign for this policy.
+    """
 
     UNKNOWN = topic_pb2.Deduplication.UNKNOWN
     NONE = topic_pb2.Deduplication.NONE
@@ -65,17 +118,88 @@ class DeduplicationStrategy(Enum):
     UNIQUE_FIELD = topic_pb2.Deduplication.UNIQUE_FIELD
 
     @classmethod
+    def convert(cls, pb_val):
+        """
+        Convert a protocol buffer descriptor or integer to the enumeration value.
+        """
+        return super(DeduplicationStrategy, cls).convert(pb_val, "Deduplication.Strategy")
+
+
+class OffsetPosition(PBEnum):
+    """
+    OffsetPosition determines where duplicates are stored in the log.
+
+    Positions
+    ---------
+    OFFSET_UNKNOWN : 0
+        Only valid if the deduplication policy is None.
+
+    OFFSET_EARLIEST : 1
+        The default deduplication offset policy: the first record in the log is
+        identified as the canonical event and all duplicate events point to this record.
+
+    OFFSET_LATEST : 2
+        An offset policy where earlier duplicates are marked as duplicates and the
+        latest event is identified as the canonical event. This policy slows down event
+        processing, but allows queries to see duplicate values sooner.
+    """
+
+    OFFSET_UNKNOWN = topic_pb2.Deduplication.OFFSET_UNKNOWN
+    OFFSET_EARLIEST = topic_pb2.Deduplication.OFFSET_EARLIEST
+    OFFSET_LATEST = topic_pb2.Deduplication.OFFSET_LATEST
+
+    __aliases__ = {
+        "EARLIEST": "OFFSET_EARLIEST",
+        "LATEST": "OFFSET_LATEST",
+    }
+
+    @classmethod
     def parse(cls, name):
+        """
+        Parse an protocol buffer enumeration value from a string (case and whitespace
+        insensitive parsing). Raises a KeyError if the string is not in the enumeration.
+        """
         name = name.strip().upper()
+        if name in cls.__aliases__:
+            name = cls.__aliases__[name]
         return cls[name]
 
     @classmethod
     def convert(cls, pb_val):
-        pb_names = topic_pb2.DESCRIPTOR.enum_types_by_name["Deduplication.Strategy"]
-        return cls[pb_names.names_by_value[pb_val]]
+        """
+        Convert a protocol buffer descriptor or integer to the enumeration value.
+        """
+        return super(OffsetPosition, cls).convert(pb_val, "Deduplication.OffsetPosition")
+
+    @classmethod
+    def __getitem__(cls, name):
+        if name in cls.__aliases__:
+            name = cls.__aliases__[name]
+        return super(OffsetPosition, cls).__getitem__(name)
 
 
-class ShardingStrategy(Enum):
+class ShardingStrategy(PBEnum):
+    """
+    ShardingStrategy specifies how events are distributed to multiple nodes.
+
+    Strategies
+    ----------
+    UNKNOWN : 0
+        This strategy is only valid if Ensign is in single node mode
+
+    NO_SHARDING : 1
+        Nodes will handle events that come to them without load balancing
+
+    CONSISTENT_KEY_HASH : 2
+        A sharding strategy that attempts to load balance events to different nodes by
+        using a consistent key hash algorithm to determine by key where the event goes.
+
+    RANDOM : 3
+        A sharding strategy that attempts to load balance by randomly selecting a node.
+
+    PUBLISHER_ORDERING : 4
+        A publisher is assigned to a single node and will always publish to that node.
+    """
 
     UNKNOWN = topic_pb2.ShardingStrategy.UNKNOWN
     NO_SHARDING = topic_pb2.ShardingStrategy.NO_SHARDING
@@ -84,11 +208,8 @@ class ShardingStrategy(Enum):
     PUBLISHER_ORDERING = topic_pb2.ShardingStrategy.PUBLISHER_ORDERING
 
     @classmethod
-    def parse(cls, name):
-        name = name.strip().upper()
-        return cls[name]
-
-    @classmethod
     def convert(cls, pb_val):
-        pb_names = topic_pb2.DESCRIPTOR.enum_types_by_name["ShardingStrategy"]
-        return cls[pb_names.names_by_value[pb_val]]
+        """
+        Convert a protocol buffer descriptor or integer to the enumeration value.
+        """
+        return super(ShardingStrategy, cls).convert(pb_val, "ShardingStrategy")
--- tests/pyensign/test_enum.py
+++ tests/pyensign/test_enum.py
@@ -1,7 +1,12 @@
 import pytest
 
 from pyensign.api.v1beta1 import topic_pb2
-from pyensign.enum import TopicState, DeduplicationStrategy, ShardingStrategy
+from pyensign.enum import (
+    TopicState,
+    DeduplicationStrategy,
+    ShardingStrategy,
+    OffsetPosition,
+)
 
 
 @pytest.mark.parametrize(
@@ -9,6 +14,7 @@ from pyensign.enum import TopicState, DeduplicationStrategy, ShardingStrategy
     [
         ("TopicState", TopicState),
         ("Deduplication.Strategy", DeduplicationStrategy),
+        ("Deduplication.OffsetPosition", OffsetPosition),
         ("ShardingStrategy", ShardingStrategy),
     ],
 )
@@ -30,3 +36,154 @@ def test_enumerations(name, enum):
     for state, value in pb_states.items():
         assert state in enum.__members__
         assert enum[state].value == value.number
+
+
+@pytest.mark.parametrize(
+    "enum,input,expected",
+    [
+        (TopicState, "undefined", TopicState.UNDEFINED),
+        (TopicState, "ready", TopicState.READY),
+        (TopicState, "ReadOnly", TopicState.READONLY),
+        (TopicState, "DELETING", TopicState.DELETING),
+        (TopicState, "  pending\t", TopicState.PENDING),
+        (TopicState, "Allocating", TopicState.ALLOCATING),
+        (TopicState, "  repairing ", TopicState.REPAIRING),
+        (DeduplicationStrategy, "unknown", DeduplicationStrategy.UNKNOWN),
+        (DeduplicationStrategy, "None", DeduplicationStrategy.NONE),
+        (DeduplicationStrategy, "STRICT", DeduplicationStrategy.STRICT),
+        (DeduplicationStrategy, "datagram", DeduplicationStrategy.DATAGRAM),
+        (DeduplicationStrategy, "key_grouped", DeduplicationStrategy.KEY_GROUPED),
+        (DeduplicationStrategy, "  unique_KEY", DeduplicationStrategy.UNIQUE_KEY),
+        (DeduplicationStrategy, "UNIQUE_field  ", DeduplicationStrategy.UNIQUE_FIELD),
+        (OffsetPosition, "earliest", OffsetPosition.OFFSET_EARLIEST),
+        (OffsetPosition, "OFFSET_earliest", OffsetPosition.OFFSET_EARLIEST),
+        (OffsetPosition, "LATEST", OffsetPosition.OFFSET_LATEST),
+        (OffsetPosition, "offset_latest", OffsetPosition.OFFSET_LATEST),
+        (ShardingStrategy, "unknown", ShardingStrategy.UNKNOWN),
+        (ShardingStrategy, "no_SHARDING", ShardingStrategy.NO_SHARDING),
+        (
+            ShardingStrategy,
+            "Consistent_KeY_hash ",
+            ShardingStrategy.CONSISTENT_KEY_HASH,
+        ),
+        (ShardingStrategy, "Random", ShardingStrategy.RANDOM),
+        (ShardingStrategy, "publisher_ordering", ShardingStrategy.PUBLISHER_ORDERING),
+    ],
+)
+def test_enumeration_parse(enum, input, expected):
+    """
+    Ensure parsing enumerations works as expected
+    """
+    assert enum.parse(input) == expected
+
+
+@pytest.mark.parametrize(
+    "enum,input,expected",
+    [
+        (TopicState, 0, TopicState.UNDEFINED),
+        (TopicState, topic_pb2.TopicState.UNDEFINED, TopicState.UNDEFINED),
+        (TopicState, 1, TopicState.READY),
+        (TopicState, topic_pb2.TopicState.READY, TopicState.READY),
+        (TopicState, 2, TopicState.READONLY),
+        (TopicState, topic_pb2.TopicState.READONLY, TopicState.READONLY),
+        (TopicState, 3, TopicState.DELETING),
+        (TopicState, topic_pb2.TopicState.DELETING, TopicState.DELETING),
+        (TopicState, 4, TopicState.PENDING),
+        (TopicState, topic_pb2.TopicState.PENDING, TopicState.PENDING),
+        (TopicState, 5, TopicState.ALLOCATING),
+        (TopicState, topic_pb2.TopicState.ALLOCATING, TopicState.ALLOCATING),
+        (TopicState, 6, TopicState.REPAIRING),
+        (TopicState, topic_pb2.TopicState.REPAIRING, TopicState.REPAIRING),
+        (DeduplicationStrategy, 0, DeduplicationStrategy.UNKNOWN),
+        (
+            DeduplicationStrategy,
+            topic_pb2.Deduplication.Strategy.UNKNOWN,
+            DeduplicationStrategy.UNKNOWN,
+        ),
+        (DeduplicationStrategy, 1, DeduplicationStrategy.NONE),
+        (
+            DeduplicationStrategy,
+            topic_pb2.Deduplication.Strategy.NONE,
+            DeduplicationStrategy.NONE,
+        ),
+        (DeduplicationStrategy, 2, DeduplicationStrategy.STRICT),
+        (
+            DeduplicationStrategy,
+            topic_pb2.Deduplication.Strategy.STRICT,
+            DeduplicationStrategy.STRICT,
+        ),
+        (DeduplicationStrategy, 3, DeduplicationStrategy.DATAGRAM),
+        (
+            DeduplicationStrategy,
+            topic_pb2.Deduplication.Strategy.DATAGRAM,
+            DeduplicationStrategy.DATAGRAM,
+        ),
+        (DeduplicationStrategy, 4, DeduplicationStrategy.KEY_GROUPED),
+        (
+            DeduplicationStrategy,
+            topic_pb2.Deduplication.Strategy.KEY_GROUPED,
+            DeduplicationStrategy.KEY_GROUPED,
+        ),
+        (DeduplicationStrategy, 5, DeduplicationStrategy.UNIQUE_KEY),
+        (
+            DeduplicationStrategy,
+            topic_pb2.Deduplication.Strategy.UNIQUE_KEY,
+            DeduplicationStrategy.UNIQUE_KEY,
+        ),
+        (DeduplicationStrategy, 6, DeduplicationStrategy.UNIQUE_FIELD),
+        (
+            DeduplicationStrategy,
+            topic_pb2.Deduplication.Strategy.UNIQUE_FIELD,
+            DeduplicationStrategy.UNIQUE_FIELD,
+        ),
+        (OffsetPosition, 0, OffsetPosition.OFFSET_UNKNOWN),
+        (
+            OffsetPosition,
+            topic_pb2.Deduplication.OffsetPosition.OFFSET_UNKNOWN,
+            OffsetPosition.OFFSET_UNKNOWN,
+        ),
+        (OffsetPosition, 1, OffsetPosition.OFFSET_EARLIEST),
+        (
+            OffsetPosition,
+            topic_pb2.Deduplication.OffsetPosition.OFFSET_EARLIEST,
+            OffsetPosition.OFFSET_EARLIEST,
+        ),
+        (OffsetPosition, 2, OffsetPosition.OFFSET_LATEST),
+        (
+            OffsetPosition,
+            topic_pb2.Deduplication.OffsetPosition.OFFSET_LATEST,
+            OffsetPosition.OFFSET_LATEST,
+        ),
+        (ShardingStrategy, 0, ShardingStrategy.UNKNOWN),
+        (
+            ShardingStrategy,
+            topic_pb2.ShardingStrategy.UNKNOWN,
+            ShardingStrategy.UNKNOWN,
+        ),
+        (ShardingStrategy, 1, ShardingStrategy.NO_SHARDING),
+        (
+            ShardingStrategy,
+            topic_pb2.ShardingStrategy.NO_SHARDING,
+            ShardingStrategy.NO_SHARDING,
+        ),
+        (ShardingStrategy, 2, ShardingStrategy.CONSISTENT_KEY_HASH),
+        (
+            ShardingStrategy,
+            topic_pb2.ShardingStrategy.CONSISTENT_KEY_HASH,
+            ShardingStrategy.CONSISTENT_KEY_HASH,
+        ),
+        (ShardingStrategy, 3, ShardingStrategy.RANDOM),
+        (ShardingStrategy, topic_pb2.ShardingStrategy.RANDOM, ShardingStrategy.RANDOM),
+        (ShardingStrategy, 4, ShardingStrategy.PUBLISHER_ORDERING),
+        (
+            ShardingStrategy,
+            topic_pb2.ShardingStrategy.PUBLISHER_ORDERING,
+            ShardingStrategy.PUBLISHER_ORDERING,
+        ),
+    ],
+)
+def test_enumeration_covert(enum, input, expected):
+    """
+    Ensure converting enumerations works as expected
+    """
+    assert enum.convert(input) == expected
